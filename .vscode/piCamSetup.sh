4e81db1505460ab1ed3e13394654253cf4c91984
rodrigonramon <rodrigo.n.ramon@gmail.com>
2016-07-14 15:21:21 -0400

    commit

---
+++ .vscode/piCamSetup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Downloads WiringPi and Builds the Code
+cd /PATH/WHERE/IT/WILL/BE/SAVED (WIRINGPI)
+git clone git://git.drogon.net/wiringPi
+cd wiringPi
+./build
+#DO GPIO SETUP FOLDERS
+
+gpio 7 out
+gpio 11 out
+gpio 12 out
+
+#ENABLE CHANNELS
+
+echo 'Setup Complete'
